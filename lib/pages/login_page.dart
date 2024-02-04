edb40f957d1b24d2ce3e8b77106610bb9f908d0a
Leonardo Barbosa <leonardo.pereira@modalgr.com.br>
2024-02-04 13:25:07 -0300

    feat: creating login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,135 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login', style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.purple,
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Sign In',
+              style: TextStyle(
+                color: Colors.purple,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  hintText: 'E-mail Adress',
+                  contentPadding: EdgeInsets.all(15.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              height: 50,
+              child: TextField(
+                obscureText: !_isPasswordVisible,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  hintText: 'Password',
+                  contentPadding: EdgeInsets.all(15.0),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Center(
+              child: Container(
+                width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Center(
+                      child: Text(
+                    'Log In',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  )),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 28),
+              child: Center(
+                  child: Text(
+                'OR',
+                style: TextStyle(color: Colors.grey[700]),
+              )),
+            ),
+            Center(
+              child: Container(
+                width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.blue[800],
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Center(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Icon(
+                          Icons.facebook_rounded,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                      ),
+                      Text(
+                        'Facebook Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
